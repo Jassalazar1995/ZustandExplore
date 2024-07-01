f9d2ef9961ed98f960c8c5825496e6701007335d
Jose Salazar <jose@mountaindev.com>
2024-07-01 13:41:00 -0400

    hi

---
+++ src/store.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand';
+
+type CounterStore = {
+    count: number;
+    increment: () => void;
+    decrement: () => void;
+}
+
+export const useCounterStore = create<CounterStore>((set) => ({
+    count: 0,
+    increment: () => {
+        set({count: 1})
+    },
+    decrement: () => {
+        set({count: -1})
+    }
+}))
